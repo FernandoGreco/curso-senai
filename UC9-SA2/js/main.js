f0e9adca2a48cc825f4a78b8bcc45c7f5b219a14
Fernando Greco <ocergfernando@gmail.com>
2022-05-04 21:14:34 -0300

    Add files via upload

--- UC9-SA2/js/main.js
+++ UC9-SA2/js/main.js
@@ -3,3 +3,17 @@
  function menu(){
     document.getElementById("menu").classList.toggle("hide")
 }
+
+/* SHOW AND HIDE FOOTER */
+
+document.getElementById('conteudo').onclick = function() {
+    this.classList.toggle('hide');
+}
+
+document.getElementById('redes').onclick = function() {
+    this.classList.toggle('hide');
+}
+
+document.getElementById('contato').onclick = function() {
+    this.classList.toggle('hide');
+}
